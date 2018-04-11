fb957137a638987113d084687351a9c0c016f0db
Stéphane Adjemian(Charybdis) <stephane.adjemian@univ-lemans.fr>
2018-04-11 19:06:18 +0200

    Updated preprocessor version.

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinaryProvider
 const verbose = "--verbose" in ARGS
 const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "usr")))
 
-PREPROCESSOR_VERSION = "70ec52a6cce2ad116b0de68eec505a4092e50a82"
+PREPROCESSOR_VERSION = "38fc19f9d33be968f29d8ab73d0633f4ea1f9c33"
 REMOTE_PATH = "https://dynare.adjemian.eu/preprocessor/$PREPROCESSOR_VERSION"
 
 products = Product[
@@ -11,11 +11,11 @@ products = Product[
 ]
 
 download_info = Dict(
-    Linux(:i686, :glibc)    => ("$REMOTE_PATH/linux/32/preprocessor.tar.gz", "dcfc2a15c35a6ee916cd0b20cec89a454082ad3aa512ea2dfd62ea2a22e58561"),
-    Linux(:x86_64, :glibc)  => ("$REMOTE_PATH/linux/64/preprocessor.tar.gz", "1f9c619c2116542c0810ae2ba8d12525993d03a46d53322e49b3fad35f6c51e7"),
-    MacOS()                 => ("$REMOTE_PATH/osx/64/preprocessor.tar.gz", "4d082dbbee46810b7350d9594f1c1d848e37c871f2fa7456be60f2847682d3bf"),
-    Windows(:i686)          => ("$REMOTE_PATH/windows/32/preprocessor.tar.gz", "e277c9dc9248ff57476612e42d7fd691ffd72cee1de4aea4926544906cc772e3"),
-    Windows(:x86_64)        => ("$REMOTE_PATH/windows/64/preprocessor.tar.gz", "5a8cbe1f58664446c28e074b2e5b79aa23c64ccd84c7b0425d2003a29292af64"),
+    Linux(:i686, :glibc)    => ("$REMOTE_PATH/linux/32/preprocessor.tar.gz", "f2256d88b02f3375636e67c36d657b089e9c7d3329d9eb192eaa8475a38d66e4"),
+    Linux(:x86_64, :glibc)  => ("$REMOTE_PATH/linux/64/preprocessor.tar.gz", "e9c04c3b1ee693e755431f57801d06cfdae9a24b11cfb02d6b1699eedd51aef4"),
+    MacOS()                 => ("$REMOTE_PATH/osx/64/preprocessor.tar.gz", "c4ad5eb94e95fdcd5c8193f099d7cbd53ecda9b044ec5ceec9506dbf21002f98"),
+    Windows(:i686)          => ("$REMOTE_PATH/windows/32/preprocessor.tar.gz", "70946d5f2c11a3d2012eb3edb96d65fffd26e1da65519b244a77f4c8a928cf78"),
+    Windows(:x86_64)        => ("$REMOTE_PATH/windows/64/preprocessor.tar.gz", "fe3f0643265d895159edb186b4ee74b766b87e544364e9e3a137a51584aa46b5"),
 )
 
 for p in products
